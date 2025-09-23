689a1e170530757422c766383c5e7281a4a5d90b
Rafialdy Mario <alfavtec@gmail.com>
2025-09-23 12:10:04 +0700

    change image assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,44 +10,47 @@ export default function HomePage() {
       <main className="flex flex-col w-full">
         <Navbar />
         <Hero />
-        <FeatureSection
-          title="Lapor"
-          description="Ajukan laporan kapan saja hanya dengan beberapa langkah sederhana. 
-    Isi data, tambahkan detail, upload foto, dan laporan Anda langsung terkirim."
-          image="/lapor.png"
-        />
-        <FeatureSection
-          title="Dashboard"
-          description="Lihat perkembangan laporan hanya dalam sekali pandang. Grafik interaktif membantu Anda memahami data lebih cepat dan akurat."
-          image="/dashboard.png"
-          reverse
-        />
-        <FeatureSection
-          title="Tiket"
-          description="Ketahui status laporan Anda. Dari proses hingga selesai, semua tiket dapat dipantau dengan mudah."
-          image="/list-tiket.png"
-        />
-        <FeatureSection
-          title="Catatan"
-          description="Setiap laporan bisa diperkuat dengan catatan tambahan, memastikan informasi tersampaikan dengan baik."
-          image="/catatan.png"
-          reverse
-        />
-        <FeatureSection
-          title="Lacak"
-          description="Pantau setiap tahapan laporan Anda, dari diterima hingga selesai dikerjakan, semua tercatat dan transparan."
-          image="/lacak-tiket.png"
-        />
-        <FeatureSection
-          title="Notifikasi"
-          description="Dapatkan notifikasi real time untuk setiap perkembangan."
-          image="/notifikasi.png"
-          reverse
-        />
+
+        {/* Wrapper dengan divider tegas */}
+        <div className="divide-y-2 divide-gray-300">
+          <FeatureSection
+            title="Lapor"
+            description="Ajukan laporan kapan saja hanya dengan beberapa langkah sederhana. 
+            Isi data, tambahkan detail, upload foto, dan laporan Anda langsung terkirim."
+            image="/lapor.png"
+          />
+          <FeatureSection
+            title="Dashboard"
+            description="Lihat perkembangan laporan hanya dalam sekali pandang. Grafik interaktif membantu Anda memahami data lebih cepat dan akurat."
+            image="/dashboard.png"
+            reverse
+          />
+          <FeatureSection
+            title="Tiket"
+            description="Ketahui status laporan Anda. Dari proses hingga selesai, semua tiket dapat dipantau dengan mudah."
+            image="/list-tiket.png"
+          />
+          <FeatureSection
+            title="Catatan"
+            description="Setiap laporan bisa diperkuat dengan catatan tambahan, memastikan informasi tersampaikan dengan baik."
+            image="/catatan.png"
+            reverse
+          />
+          <FeatureSection
+            title="Lacak"
+            description="Pantau setiap tahapan laporan Anda, dari diterima hingga selesai dikerjakan, semua tercatat dan transparan."
+            image="/lacak-tiket.png"
+          />
+          <FeatureSection
+            title="Notifikasi"
+            description="Dapatkan notifikasi real time untuk setiap perkembangan."
+            image="/notifikasi.png"
+            reverse
+          />
+        </div>
         <Contact />
         <Footer />
       </main>
     </div>
-
   );
 }
